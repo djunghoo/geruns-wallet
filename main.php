e00c809333c399f8948de48576d548341e260a14
djunghoo <84010885+djunghoo@users.noreply.github.com>
2024-03-14 09:15:33 +0700

    update v1
    จัดการโครงสร้าง Mod_rewrite
    nginx

--- main.php
+++ main.php
@@ -1,9 +1,34 @@
-<?php $part = isset($_SERVER['PATH_INFO']) ? explode('/', preg_replace('~^/?(.*?)/?
-	$~', '$1', $_SERVER['PATH_INFO'])) : array();
-$mod_co = count($part);
-$web_file = 'app_modules/';
-$web_folder = 'app_modules/';
-$view = array(
-	"home" => $web_folder . "/"
-);
-$page = array('login', 'logout');
+<?php 
+$webFolder = 'app_modules/';
+//config route 
+$view = [
+	"home" => [
+		"appFolder" => $webFolder . "home/",
+		"appFile" => $webFolder . "home/index.php",
+		"title" => "หน้าหลัก"
+	],
+	"products" => [
+		"appFolder" => $webFolder . "products/",
+		"appFile" => $webFolder . "products/index.php",
+		"title" => "สินค้า"
+	],
+];
+//config 
+$web = [
+	"domain" => "https://geruns-wallet.azurewebsites.net",
+];
+
+$menu = [
+	"home" => [
+		"type" => "Link",
+		"icon" => "fa fa-file",
+		"title" => "หน้าหลัก",
+		"url" => "home",
+	],
+	"products" => [
+		"type" => "Link",
+		"icon" => "fa fa-file",
+		"title" => "สินค้า",
+		"url" => "products",
+	]
+];
